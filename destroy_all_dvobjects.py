13e445b062f752b5b028c9d3b03fb4cafe18659d
Philip Durbin <philip_durbin@harvard.edu>
2019-06-26 13:21:41 -0400

    have destroy_all_dvobjects.py actually delete more

--- destroy_all_dvobjects.py
+++ destroy_all_dvobjects.py
@@ -4,21 +4,38 @@ import dvconfig
 base_url = dvconfig.base_url
 api_token = dvconfig.api_token
 api = Api(base_url, api_token)
-print(api.status)
-#FIXME: remove hard coded dataverse alias
-alias = 'open-source-at-harvard'
-query_str = '/dataverses/' + alias + '/contents'
-params = {}
-params['foo'] = 'bar'
-resp = api.get_request(query_str, params=params, auth=True)
-print(json.dumps(resp.json(), indent=2))
-# FIXME: iterate through all content instead of just looking at index 0.
-protocol = resp.json()['data'][0]['protocol']
-authority = resp.json()['data'][0]['authority']
-identifier = resp.json()['data'][0]['identifier']
-dataset_pid = protocol + ':' + authority + '/' + identifier
-print(dataset_pid)
-resp = api.delete_dataset(dataset_pid)
-print(resp)
-resp = api.delete_dataverse(alias)
-print(resp)
+print('API status: ' +api.status)
+
+dataverse_ids = []
+dataset_ids = []
+
+def main():
+    print("Finding dataverses and datasets to destroy...")
+    find_children(1)
+    dataset_ids.sort(reverse=True)
+    for dataset_id in dataset_ids:
+        print('Deleting dataset id ' + str(dataset_id))
+        resp = api.delete_dataset(dataset_id, is_pid=False)
+        print(resp)
+    dataverse_ids.sort(reverse=True)
+    for dataverse_id in dataverse_ids:
+        print('Deleting dataverse id ' + str(dataverse_id))
+        resp = api.delete_dataverse(dataverse_id)
+        print(resp)
+    print("Done.")
+
+def find_children(dataverse_database_id):
+    query_str = '/dataverses/' + str(dataverse_database_id) + '/contents'
+    params = {}
+    resp = api.get_request(query_str, params=params, auth=True)
+    for dvobject in resp.json()['data']:
+        dvtype = dvobject['type']
+        dvid = dvobject['id']
+        if 'dataverse' == dvtype:
+            find_children(dvid)
+            dataverse_ids.append(dvid)
+        else:
+            dataset_ids.append(dvid)
+
+if __name__ == '__main__':
+    main()
