e935f256e107945245ab2fe0d516e08d981a7f10
Philip Durbin <philip_durbin@harvard.edu>
2019-11-14 12:33:40 -0500

    start working with 2018 data

--- to_packed_circles.py
+++ to_packed_circles.py
@@ -2,16 +2,22 @@ import csv
 import json
 from collections import defaultdict
 '''
+fileid
 filename
 dataset_name
+dataverse_level_1_id
 dataverse_level_1_alias
-dataverse_level_1_friendly
+dataverse_level_1_friendly_name
+dataverse_level_2_id
 dataverse_level_2_alias
-dataverse_level_2_friendly
+dataverse_level_2_friendly_name
+dataverse_level_3_id
 dataverse_level_3_alias
-dataverse_level_3_friendly
+dataverse_level_3_friendly_name
 subjects
-publication_date
+file_creation_date
+file_publication_date
+dataset_publication_date
 '''
 data = defaultdict(dict)
 final = {}
@@ -23,9 +29,12 @@ with open('files.tsv', newline='') as tsvfile:
     rows = [row for row in reader]
     for row in rows:
         filename = row['filename']
-        dv1name = row['dataverse_level_1_friendly']
-        dv2name = row['dataverse_level_2_friendly']
-        dv3name = row['dataverse_level_3_friendly']
+        dv1name = row['dataverse_level_1_friendly_name']
+        dv1id = row['dataverse_level_1_id']
+        dv2name = row['dataverse_level_2_friendly_name']
+        dv2id = row['dataverse_level_2_id']
+        dv3name = row['dataverse_level_3_friendly_name']
+        dv3id = row['dataverse_level_3_id']
         title = row['dataset_name']
         #print("%-20s > %-20s > %-20s > %-20s > %-20s" % (dv1name[:20], dv2name[:20], dv3name[:20], title[:20], filename[:20]))
         if dv3name:
@@ -55,6 +64,7 @@ with open('files.tsv', newline='') as tsvfile:
                 seen[dv1name + title] = 1
 data_out = json.dumps(data, indent=2)
 #print(data_out)
+#exit(1)
 #for child_of_root in data:
 # for key, value in ourNewDict.items():
 
@@ -62,23 +72,33 @@ data_out = json.dumps(data, indent=2)
 for corkey, corval in data.items():
     #print(corkey)
     level1 = {}
+    #level1['name'] = corkey + '-level1'
     level1['name'] = corkey
+    level1['children'] = []
     # gcor = "grandchild of root"
     for gcorkey, gcorval in corval.items():
+        #print(gcorkey)
         # ggcor = "great grandchild of root"
-        if isinstance(gcorval,dict):
-        #if gcorval.items():
-            for ggcorkey, ggcorval in gcorval.items():
-                #print('ggcorkey:', ggcorkey)
-                level3 = {}
-                level3['name'] = gcorkey
-                level2['children'] = []
-                level2['children'].append(level3)
+        #level2 = {}
+#        if isinstance(gcorval,dict):
+#        #if gcorval.items():
+#            for ggcorkey, ggcorval in gcorval.items():
+#                #print('ggcorkey:', ggcorkey)
+#                level3 = {}
+#                #if isinstance(ggcorval,dict):
+#                #    for gggcorkey, gggcorval in ggcorval.items():
+#                level3['name'] = ggcorkey + '-level3'
+#                level2['children'] = []
+#                level2['children'].append(level3)
         #print(gcorkey)
         level2 = {}
+        #level2['name'] = gcorkey + '-level2'
         level2['name'] = gcorkey
-        level1['children'] = []
+        #level1['children'] = []
+        #if (level2):
+        #    level1['children'].append(level2)
         level1['children'].append(level2)
+#            level2 = {}
     final['children'].append(level1)
 final_out = json.dumps(final, indent=2)
 print(final_out)
